87afc1c98de040637935bda09049be3a532454d6
Kaleab Alemu <kaleabtesfaye01@gmail.com>
2023-11-08 22:58:04 -0500

    rename files and add Task2 feature

--- group_2_Lab7_parts2n3.py
+++ group_2_Lab7_parts2n3.py
@@ -2,8 +2,11 @@
 Name: Kaleab Alemu and Manogya Aryal
 DSC 200
 Lab 7: Web Scraping
-Description:
-
+Description:    This code addresses a web scraping assignment with two key tasks. Firstly, it implements a function for
+                one website to retrieve allowed web scraped pages, saving the contents in a CSV file following a
+                specified naming convention. Secondly, it includes separate functions to scrape data from two given
+                URLs, utilizing pandas, beautifulsoup4, and requests, and printing the dataset dimensions. File names
+                follow a format indicating the group name and data source.
 Due Date: Nov 8, 2023
 """
 
@@ -14,6 +17,33 @@ from bs4 import BeautifulSoup
 
 
 class Lab6:
+    def task2(self):
+        url = "https://www.ebay.com/sch/i.html?_nkw=nike_ul"
+
+        page = req.get(url)
+
+        page_parsed = BeautifulSoup(page.content, 'html.parser')
+        header = ["Product", "Condition", "Price", "Shipping Fee", "Image"]
+        data = [header]
+
+        # Find all relevant elements
+        product = page_parsed.find("div", "srp-river-results clearfix")
+        product_items = product.find_all("li", "s-item s-item__pl-on-bottom")
+
+        for item in product_items:
+            title_value = item.find("div", "s-item__title").text
+            price_value = item.find("span", "s-item__price").text
+            condition = item.find("span", "SECONDARY_INFO").text
+            shipping = item.find("span", "s-item__shipping s-item__logisticsCost")
+            if shipping:
+                shipping = shipping.text
+            image_div = item.find("div", "s-item__image-wrapper image-treatment")
+            image = image_div.find("img").get("src")
+            data.append([title_value, condition, price_value, shipping, image])
+
+        # write the extracted data to a csv file
+        self.write_csv('group_2_task2.csv', data)
+
     def task3a(self):
         # url to scrape from
         url = "https://vincentarelbundock.github.io/Rdatasets/datasets.html"
@@ -60,7 +90,10 @@ class Lab6:
                 rows.append(row)
 
         # write the extracted data to a csv file
-        self.write_csv('part3a-output.csv', rows)
+        self.write_csv('part3a-group2-output.csv', rows)
+
+        # print number of rows and columns
+        print('Part3-a has %d rows and %d columns' % (len(rows), len(rows[0])))
 
     def task3b(self):
         # url to scrape
@@ -97,7 +130,10 @@ class Lab6:
             data.append([job_title, company_name, city, state, posting_date])
 
         # write the extracted job data to a CSV file
-        self.write_csv('part3b-output.csv', data)
+        self.write_csv('part3b-group2-output.csv', data)
+
+        # print number of rows and columns
+        print('Part3-b has %d rows and %d columns' % (len(data), len(data[0])))
 
     @staticmethod
     def write_csv(filename, data):
@@ -108,12 +144,19 @@ class Lab6:
         fptr.close()
 
 
-if __name__ == '__main__':
+def main():
     # Create an instance of the Lab7 class
     lab6 = Lab6()
+
+    # Execute the task2 method to scrape data and write it to a CSV file
+    lab6.task2()
 
     # Execute the task3a method to scrape data and write it to a CSV file
     lab6.task3a()
 
     # Execute the task3b method to scrape job data and write it to a CSV file
     lab6.task3b()
+
+
+if __name__ == '__main__':
+    main()
